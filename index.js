9ae77e44817fff623699c8727558982f05dd0f01
gaburibe <gaburibe@gmail.com>
2020-05-19 10:31:15 -0500

    cerrar mercado

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/menu', function (req, res) {
 //SITIO PRINCIPAL
 app.get('/', function (req, res) {
 	console.log("--")
-	res.sendFile(path.join(__dirname + '/sitio/menu.html'));
+	res.sendFile(path.join(__dirname + '/sitio/splash.html'));
 
 });
 app.get('/orden', function (req, res) {
