24159c6046694b444a6e4e69ede2b15ace54d3d4
gaburibe <gaburibe@gmail.com>
2020-06-06 20:36:18 -0500

    fuck

--- menuManager.js
+++ menuManager.js
@@ -11,7 +11,7 @@ var csv = require("fast-csv");
 
 exports.liveMenu={};
 exports.cache=false;
-exports.currentMenu="menu30mayo";
+exports.currentMenu="menu16mayo";
 exports.inicializa=function (){
 	isCached=getMostRecentFileName("menu/workingcopy");
 	if(isCached!="menu.json"){
